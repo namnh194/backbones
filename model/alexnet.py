f609669f662c06629eaf20f27ab6b8538ecd4faf
namnh194 <handsomeboy1942000@gmail.com>
2023-07-09 18:39:29 +0700

    update alexnet

--- model/alexnet.py
+++ model/alexnet.py
@@ -4,19 +4,53 @@ import numpy as np
 from torchvision import transforms, datasets
 
 class AlexNet(nn.Module):
-    def __init__(self, n_channels=3):
+    def __init__(self, num_classes):
         super(AlexNet, self).__init__()
         self.block1 = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4, padding=0),
             nn.BatchNorm2d(96),
-            nn.ReLU()
-        )
-
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2))
+        self.block2 = nn.Sequential(
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2))
+        self.block3 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(384),
+            nn.ReLU())
+        self.block4 = nn.Sequential(
+            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(384),
+            nn.ReLU())
+        self.block5 = nn.Sequential(
+            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2))
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(9216, 4096),
+            nn.ReLU())
+        self.fc2 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU())
+        self.cls = nn.Linear(4096, num_classes)
     def forward(self, x):
-        _ = self.block1(x)
-        return _
+        out = self.block1(x)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.block4(out)
+        out = self.block5(out)
+        out = out.reshape(out.size(0), -1) # batch_size x ...
+        out = self.fc1(out)
+        out = self.fc2(out)
+        out = self.cls(out)
+        return out
 
 if __name__ == "__main__":
-    net = AlexNet()
-    x = torch.rand((1,3,20,20), dtype=torch.float)
+    net = AlexNet(num_classes=10)
+    x = torch.rand((1,3,256,256), dtype=torch.float)
     print(net(x))
---
+++ train/alexnet_cifar10.py
@@ -0,0 +1,57 @@
+from dataset.cifar10 import *
+import torch
+from model.alexnet import *
+
+
+# hyperparam
+num_classes = 10
+num_epochs = 20
+batch_size = 64
+learning_rate = 0.005
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+train_loader, valid_loader = get_train_valid_loader(data_dir='./data', batch_size=64,
+                                                    augment=False, random_seed=1)
+test_loader = get_test_loader(data_dir='./data',
+                              batch_size=64)
+model = AlexNet(num_classes).to(device)
+
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.005)
+
+
+# Train the model
+total_step = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # Move tensors to the configured device
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+          .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+
+    # Validation
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in valid_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            del images, labels, outputs
+
+        print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total))
