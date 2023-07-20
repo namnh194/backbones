5ccd6fffb8ca35e32122e17248f3f09e12e8aea4
namnh194 <handsomeboy1942000@gmail.com>
2023-07-20 17:02:32 +0700

    add detnet

---
+++ model/detnet.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Detnet_block(nn.Module):
+    def __init__(self):
+        super(Detnet_block, self).__init__()
+    def forward(self, x):
+        return
+
+class Detnet(nn.Module):
+    def __init__(self):
+        super(Detnet, self).__init__()
+    def forward(self, x):
+        return
+
+if __name__ == "__main__":
+    pass
