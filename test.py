808d45966ece98c0c5ac3fee5130ac7432fb28ab
namnh194 <handsomeboy1942000@gmail.com>
2023-08-14 13:45:34 +0700

    add sth

--- test.py
+++ test.py
@@ -7,3 +7,4 @@ shufflenet = models.shufflenet_v2_x1_0(pretrained=True)
 x = torch.randn((1, 3, 224, 224), dtype=torch.float)
 print(sum(p.numel() for p in shufflenet.parameters() if p.requires_grad))
 print(shufflenet(x).shape)
+layer = nn.GRU
